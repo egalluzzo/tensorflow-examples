05c64d13057f7117492a190fac3b5605ee60a3a0
Eric Galluzzo <egalluzzo@gmail.com>
2016-07-29 08:20:15 -0400

    Do a little renaming

--- examples/simple_neural_network.py
+++ examples/simple_neural_network.py
@@ -8,27 +8,28 @@ def softmax(vec):
 	total = sum(exp)
 	return [elem / total for elem in exp]
 
-class HiddenLayer:
-	def __init__(self, n_inputs, n_outputs, activation):
-		self.n_inputs = n_inputs
-		self.n_outputs = n_outputs
+class NeuralNetworkLayer:
+	def __init__(self, input_size, output_size, activation):
+		self.input_size = input_size
+		self.output_size = output_size
 		self.activation = activation
-		self.weights = [[random.uniform(0, 1) for x in range(0, n_inputs)] for y in range(0, n_outputs)]
-		self.bias = [random.uniform(0, 1) for y in range(0, n_outputs)]
+		self.weights = [[random.uniform(0, 1) for x in range(0, input_size)] for y in range(0, output_size)]
+		self.bias = [random.uniform(0, 1) for y in range(0, output_size)]
 
 	def apply(self, inputs):
 		outputs = []
-		for i in range(self.n_outputs):
+		for i in range(self.output_size):
 			sum = 0
-			for j in range(self.n_inputs):
+			for j in range(self.input_size):
 				sum += self.weights[i][j] * inputs[j]
 			sum += self.bias[i]
 			outputs.append(sum)
 		return self.activation(outputs)
 
+
 inputs = [1, 2, 3]
-hidden_layer = HiddenLayer(3, 4, sigmoid)
-output_layer = HiddenLayer(4, 2, softmax)
+hidden_layer = NeuralNetworkLayer(3, 4, sigmoid)
+output_layer = NeuralNetworkLayer(4, 2, softmax)
 outputs = output_layer.apply(hidden_layer.apply(inputs))
 print("Input: {}".format(inputs))
 print("Hidden layer:\n  Weights: {}\n  Bias: {}".format(hidden_layer.weights, hidden_layer.bias))
